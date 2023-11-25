606298dac57bb47c4d135e1c8f65ac9dc3084a9c
Ashraf266 <moashraf2662@gmail.com>
2023-11-25 09:36:36 +0200

    port driver is Done

--- MCAL/port/port.c
+++ MCAL/port/port.c
@@ -25,18 +25,54 @@
 *******************************************************************/
 void PORT_init(PORT_ConfigType *Config)
 {
+    u8 pinID;
     u8 pinNumber;
     u8 portNumber;
-    for(pinNumber = 0; pinNumber < PORT_NUM_OF_PINS; pinNumber++)
+
+    /* Initialize each Pin */
+    for(pinID = 0; pinID < PORT_NUM_OF_PINS; pinID++)
     {
-        if((pinNumber = 13) || (pinNumber = 14) || (pinNumber = 18) || (pinNumber > 31))
+        /* Skip those pins it's for dubugging */
+        if((pinID = 13) || (pinID = 14) || (pinID = 18) || (pinID > 31))
         {
             continue;
         }
-        portNumber = pinNumber/16;
-
-
         
+        /* get port & pin numbers*/
+        portNumber = pinID/16;
+        pinNumber  = pinID - portNumber*16;
+
+        /* set cfg & mode */
+        if (pinNumber < 8 )
+        {
+            /* Clear OLD Configurations */
+            PORT[portNumber]->CRL &= ~((u32)0xF<<(pinNumber*4));
+
+            /* Set NEW Configurations */
+            PORT[portNumber]->CRL |= ((u32)((Config->pins[pinID].pinCfg << 2) || Config->pins[pinID].pinMode )<<(pinNumber*4));
+        }
+        else
+        {
+            /* Clear OLD Configurations */
+            PORT[portNumber]->CRH &= ~((u32)0xF<<(pinNumber*4));
+
+            /* Set NEW Configurations */
+            PORT[portNumber]->CRH |= ((u32)((Config->pins[pinID].pinCfg << 2) || Config->pins[pinID].pinMode )<<(pinNumber*4));
+        }
+
+        /* Check pin direction to set additional configuration like internal res & Initial pin level*/
+        if(Config->pins[pinID].pinDirection == PORT_PIN_OUTPUT)
+        {
+            /* initialize pin level */
+            CLR_BIT(PORT[portNumber]->ODR, pinNumber);
+            PORT[portNumber]->ODR |= (Config->pins[pinID].pinDirection << pinNumber);
+        }
+        else
+        {
+            /* configure the internal resistor */
+            PORT[portNumber]->ODR |= (Config->pins[pinID].pinInternalResistor << pinNumber);
+        }
+
 
     }
 }
