b21a975aeae368d4e209f1d51ef039e3430ecc0d
Ashraf266 <mohamedashrafece@gmail.com>
2023-12-11 23:27:10 +0200

    PWM Driver File Structure

---
+++ MCAL/pwm/pwm_private.h
@@ -0,0 +1,13 @@
+/********************************************************************
+ * Module       : pwm                                               *
+ * File Name    : pwm_private.h                                     *
+ * Description  : contains private definitions & types              *
+ * Author       : Mohamed Ashraf                                    *
+ ********************************************************************/
+
+#ifndef PWM_PRIVATE_H
+#define PWM_PRIVATE_H
+
+
+
+#endif /* PWM_PRIVATE_H */
