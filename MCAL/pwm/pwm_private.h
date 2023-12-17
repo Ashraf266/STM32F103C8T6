16ef5574d6a8bac8c42c41a74b399b1662524beb
Ashraf266 <mohamedashrafece@gmail.com>
2023-12-18 01:57:22 +0200

    Modifying the Register Map

--- MCAL/pwm/pwm_private.h
+++ MCAL/pwm/pwm_private.h
@@ -28,12 +28,12 @@ typedef struct
     u32 CNT;
     u32 PSC;
     u32 ARR;
-    u32 reserved_1; /* Not reserved in TIM1 but we won't use it in this Driver */
+    u32 RCR; /* Only Available in TIM1 Reserved in Other Timers*/
     u32 CCR1;
     u32 CCR2;
     u32 CCR3;
     u32 CCR4;
-    u32 reserved_2; /* Not reserved in TIM1 but we won't use it in this Driver */
+    u32 BDTR; /* Only Available in TIM1 Reserved in Other Timers*/
     u32 DCR;
     u32 DMAR;
 	
